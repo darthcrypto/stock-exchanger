dfb58e4f00b73aa8be4c6f6a193feede2cf1f7d6
Michael Elgayar <michaelelgayar@michael.fios-router.home>
2019-01-12 09:58:06 -0500

    Basic berkshire mat card on homepage

--- stock-exchanger/src/app/stocks/berkshire/berkshire.component.ts
+++ stock-exchanger/src/app/stocks/berkshire/berkshire.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-stocks-berkshire',
+    selector: 'app-berkshire',
     templateUrl: './berkshire.component.html'
 })
 
