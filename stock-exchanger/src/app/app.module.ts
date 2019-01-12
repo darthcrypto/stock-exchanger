13467220df23653edadd56689fed6d817c7da5a8
Michael Elgayar <michaelelgayar@michael.local>
2019-01-12 08:43:24 -0500

    Add component for header, import component into app.module.ts

--- stock-exchanger/src/app/app.module.ts
+++ stock-exchanger/src/app/app.module.ts
@@ -6,15 +6,17 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
