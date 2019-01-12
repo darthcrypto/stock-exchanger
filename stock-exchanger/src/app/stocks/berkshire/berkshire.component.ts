a082e2ca38187be9d82615cfa1de6966b5b09b3d
Michael Elgayar <michaelelgayar@michael.fios-router.home>
2019-01-12 09:49:40 -0500

    Add berkshire directory and skeleton code, add MatCardModule

---
+++ stock-exchanger/src/app/stocks/berkshire/berkshire.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-stocks-berkshire',
+    templateUrl: './berkshire.component.html'
+})
+
+export class BerkshireComponent {}
