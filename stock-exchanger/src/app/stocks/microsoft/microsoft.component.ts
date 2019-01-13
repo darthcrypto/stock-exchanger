53c8ef49b23f87aae64b35a1a2fe3ebb5526eff3
Michael Elgayar <michaelelgayar@michael.fios-router.home>
2019-01-13 16:21:36 -0500

    Add stock components to app module

---
+++ stock-exchanger/src/app/stocks/microsoft/microsoft.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-microsoft',
+    templateUrl: './microsoft.component.html',
+    styleUrls: ['./microsoft.component.css']
+})
+
+export class MicrosoftComponent {}
