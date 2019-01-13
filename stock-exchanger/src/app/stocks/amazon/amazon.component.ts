feb0e3ffed3c9c1f1a08557a16a34b0d7dfe2bd6
Michael Elgayar <michaelelgayar@michael.fios-router.home>
2019-01-13 15:03:42 -0500

    Import amazon component into app module

---
+++ stock-exchanger/src/app/stocks/amazon/amazon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-amazon',
+    templateUrl: './amazon.component.html',
+    styleUrls: ['./amazon.component.css']
+})
+
+export class AmazonComponent {}
