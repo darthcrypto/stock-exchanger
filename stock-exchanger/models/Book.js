80a8d5865f34686eb18ee9886e3068348a7068d1
Michael Elgayar <michaelelgayar@michael.local>
2019-01-19 16:59:37 -0500

    Add data routes

--- stock-exchanger/models/Book.js
+++ stock-exchanger/models/Book.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var BookSchema = new mongoose ({
+var BookSchema = new mongoose.Schema({
     isbn: String,
     title: String,
     author: String,
@@ -8,6 +8,7 @@ var BookSchema = new mongoose ({
     published_year: String,
     publisher: String,
     updated_date: { type: Date, default: Date.now },
-});
+  });
 
-module.exports = mongoose.model('Book', BookSchema);
+  module.exports = mongoose.model('Book', BookSchema);
+  
